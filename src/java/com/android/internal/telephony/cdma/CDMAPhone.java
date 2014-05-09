664e9013f696bf81dc5cfa5cea155e7ffc825419
mksy <maksik.boy@gmail.com>
2014-05-10 01:43:30 +0300

    move the default value of voicemail number into build.prop
    Change-Id: Ia93e49fc5dda83eb9390b0ce87399ab907873346

--- src/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ src/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -847,14 +847,13 @@ public class CDMAPhone extends PhoneBase {
     public String getVoiceMailNumber() {
         String number = null;
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
-        // TODO: The default value of voicemail number should be read from a system property
 
         // Read platform settings for dynamic voicemail number
         if (getContext().getResources().getBoolean(com.android.internal
                 .R.bool.config_telephony_use_own_number_for_voicemail)) {
             number = sp.getString(VM_NUMBER_CDMA, getLine1Number());
         } else {
-            number = sp.getString(VM_NUMBER_CDMA, "*86");
+            number = sp.getString(VM_NUMBER_CDMA, SystemProperties.get("ro.cdma.vm.number"));
         }
         return number;
     }
